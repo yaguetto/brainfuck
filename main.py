895da9d3a341da761c9b4e2dbeb06f150936908c
Yago Rogatto Leme <Yaguetto@Yagos-MacBook-Pro.local>
2019-01-05 19:19:44 -0200

    criando o interpretador

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import sys
 
 def main():
-    ler_arquivo(sys.argv[1])
+    codigo = ler_arquivo(sys.argv[1])
+    interpretar(codigo)
 
 def ler_arquivo(arquivo):
     arquivo_lido = open(arquivo, 'r')
     conteudo = arquivo_lido.read()
     arquivo_lido.close()
     return conteudo
+
+def verificar_caractere_invalido(caractere):
+    return caractere != '>' and caractere != '<' and caractere != '+' and caractere != '-' and caractere != ',' and caractere != '.' and caractere != '[' and caractere != ']'
+
+def criar_memoria():
+    memoria = []
+    for i in range(0,30000):
+        memoria.append(0)
+    return memoria
+
+def interpretar(codigo):
+    memoria = criar_memoria()
+    ponteiro = 0
+    for caractere in codigo:
+        if verificar_caractere_invalido(caractere):
+            continue
+        if caractere == '>':
+            ponteiro = (ponteiro + 1) % len(memoria)
+        if caractere == '<':
+            ponteiro = (ponteiro - 1) % len(memoria)
+        if caractere == '+':
+            memoria[ponteiro] = (memoria[ponteiro] + 1) % 256
+        if caractere == '-':
+            memoria[ponteiro] = (memoria[ponteiro] - 1) % 256
+        if caractere == '.':
+            sys.stdout.write(chr(memoria[ponteiro]))    
+    print()
 
 if __name__ == '__main__':
     main()
