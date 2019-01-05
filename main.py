f194a9769f5073b8083905985724a7be58019666
Yago Rogatto Leme <Yaguetto@Yagos-MacBook-Pro.local>
2019-01-05 18:12:07 -0200

    criando funcao para ler o arquivo

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
+
 def main():
-    pass
+    ler_arquivo(sys.argv[1])
+
+def ler_arquivo(arquivo):
+    arquivo_lido = open(arquivo, 'r')
+    conteudo = arquivo_lido.read()
+    arquivo_lido.close()
+    return conteudo
 
 if __name__ == '__main__':
     main()
