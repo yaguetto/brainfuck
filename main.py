ce27c30a11339c4b39da9a6c5956d34d5a897e11
Yago Rogatto Leme <yaguetto@Yagos-Macbook.local>
2019-01-13 15:32:03 -0200

    criando loo com
    [

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def ler_arquivo(arquivo):
     return conteudo
 
 def verificar_caractere_invalido(caractere):
-    return caractere != '>' and caractere != '<' and caractere != '+' and caractere != '-' and caractere != ',' and caractere != '.' and caractere != '[' and caractere != ']'
+    return caractere != '>' and caractere != '<' and caractere != '+' and caractere != '-' and caractere != ',' and caractere != '.' and caractere != '[' and caractere != ']' and caractere != ';'
 
 def criar_memoria():
     memoria = []
@@ -22,13 +22,14 @@ def criar_memoria():
 def interpretar(codigo):
     memoria = criar_memoria()
     ponteiro = 0
+    controle_loop = []
     indice_caractere = 0
-    stack_controle = []
     while indice_caractere < len(codigo):
         caractere = codigo[indice_caractere]
         if verificar_caractere_invalido(caractere):
             indice_caractere = indice_caractere + 1
             continue
+        #print(indice_caractere, caractere)
         if caractere == '>':
             ponteiro = (ponteiro + 1) % len(memoria)
         if caractere == '<':
@@ -39,7 +40,30 @@ def interpretar(codigo):
             memoria[ponteiro] = (memoria[ponteiro] - 1) % 256
         if caractere == '.':
             sys.stdout.write(chr(memoria[ponteiro]))
-        indice_caractere = indice_caractere + 1
+            sys.stdout.flush()
+        if caractere == ';':
+            print(memoria[ponteiro])
+        if caractere == '[':
+            if memoria[ponteiro] != 0:
+                controle_loop.append(indice_caractere)
+            if memoria[ponteiro] == 0:
+                contador_loop = 1
+                while contador_loop != 0:
+                    indice_caractere += 1
+                    if codigo[indice_caractere] == '[':
+                        contador_loop += 1
+                    if codigo[indice_caractere] == ']':
+                        contador_loop -= 1
+                #print(indice_caractere, ']')
+                #indice_caractere += 1
+        if caractere == ']':
+            x = controle_loop.pop()
+            indice_caractere = x
+        if caractere == ',':
+            proxima_tecla = sys.stdin.read(1)
+            memoria[ponteiro] = ord(proxima_tecla)
+        if caractere != ']':
+            indice_caractere = indice_caractere + 1
     print()
 
 if __name__ == '__main__':
