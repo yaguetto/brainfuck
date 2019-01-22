0521082f2d1e946d7ef1efd94e9c108e6a4b16e8
Yago Rogatto Leme <yago.r.leme@gmail.com>
2019-01-22 21:43:43 -0200

    criando o arquivo de interface em HTML e abrindo ele no servidor

--- server.py
+++ server.py
@@ -1,35 +1,12 @@
 import interpretador
+import codecs
 from flask import Flask
 app = Flask(__name__)
 
-index = '''
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8" />
-    <title>Brainfuck interpreter</title>
-    <h1>Brainfuck interpreter</h1>
-    <p>Interpretador de Brainfuck desenvolvido por Gustavo Kawamoto e Yago Rogatto.</p>
-</head>
-<body>
-    <form action="/" method="POST">
-        <div>
-            <label for="code">Código:</label>
-            <br>
-                <textarea name="code"></textarea>
-        </div>
-            <input type="submit">
-    </form>
 
-    <dl>
-        <dt>GitHub Gustavo:</dt>
-        <dd><li><a href = "https://github.com/gkawamoto">Gkawamoto</a></li></dd>
-        <dt>GitHub Yago:</dt>
-        <dd><li><a href = "https://github.com/yaguetto">Yaguetto</a></li></d>
-    </dl>
-</body>
-</html>>
-'''
+abrir_html = codecs.open("interface.html", 'r', 'utf-8')
+index = abrir_html.read()
+abrir_html.close()
 
 @app.route("/")
 def serve_index():
